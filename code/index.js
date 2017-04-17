fe6f99c0a3c5968e6e5b4456213d5950656d6eb3
nicholasroberson <roberson@grinnell.edu>
2017-04-17 15:41:49 -0500

    added data loading function

--- code/index.js
+++ code/index.js
@@ -11,3 +11,13 @@ margins = 30;
 
 // parse by year-month-day
 var parseTime = d3.timeParse("%y-%b-%d");
+
+
+d3.queue()
+  .defer( d3.csv, 'data/UNITED_STOCK.csv' )
+  .defer( d3.csv, 'data/UNITED_GOOGLE_TRENDS.csv' )
+  .await( function(error, untied_stock, united_google_trends ) {
+
+    // process data here!
+
+  });
