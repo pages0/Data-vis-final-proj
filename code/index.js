88c4e81be27b029f425bfac3f483393e951d2337
pages0 <reilly.grant@gmail.com>
2017-04-25 19:41:54 -0500

    updated data for easier parsing, and starting implementing data processing

--- code/index.js
+++ code/index.js
@@ -1,21 +1,44 @@
 
 /* Created  on 4-17-17 by Nick Roberson, Reilly Grant, and Connor GT */
+// Used https://bl.ocks.org/mbostock/3883245
 
+d3.select("body").append("svg").attr("width", 1000).attr("height",600);
+
+var svg = d3.select("svg"),
+    margin = {top: 20, right: 20, bottom: 30, left: 50},
+    svg_width = +svg.attr("width") - margin.left - margin.right,
+    svg_height = +svg.attr("height") - margin.top - margin.bottom,
+    g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 //height and width of svg's
-var svg_height = 400;
-var svg_width = 1000;
-// margins for the left, top, right, and bottom.
-var margins = 30;
 
 // parse by year-month-day
 var parseTime = d3.timeParse("%y-%b-%d");
+
+var scaleTime = d3.scaleTime().rangeRound([0,svg_width]);
+
+var scaleStock = d3.scaleLinear().rangeRound([svg_height, 0]);
+var scaleSocial = d3.scaleLinear().rangeRound([svg_height, 0]);
+
+var stockLine = d3.line()
+    .x(function(d) { return scaleTime(d.Date);} )
+    .y(function(d) { return scaleStock(d.Close);});
 
 // queue data for loading here 
 d3.queue()
   .defer( d3.csv, 'data/UNITED_STOCK.csv' )
   .defer( d3.csv, 'data/UNITED_GOOGLE_TRENDS.csv' )
-  .await( function(error, untied_stock, united_google_trends ) {
+  .await( function(error, united_stock, united_google_trends ) {
 
     // process data here!
+      for (d in united_stock){
+	  d.Date = parseTime(d.Date);
+      }
+      for (d in united_google_trends){
+	  d.Date = parseTime(d.Date);
+      }
 
+
+
+
+    
   });
