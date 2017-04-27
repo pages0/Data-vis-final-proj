e6367a23c0354a226b7bde480ab0a46de62f8596
nicholasroberson <roberson@grinnell.edu>
2017-04-27 15:31:24 -0500

    deleted unimportant functions

--- code/index.js
+++ code/index.js
@@ -30,64 +30,64 @@ d3.queue()
 function displayData(FileName) {
     d3.select("svg").remove();
     d3.select('#svg_area').append("svg").attr("width", 800).attr("height",450);
-    
+
     var svg = d3.select("svg"),
 	margin = {top: 20, right: 50, bottom: 30, left: 50},
 	svg_width = +svg.attr("width") - margin.left - margin.right,
 	svg_height = +svg.attr("height") - margin.top - margin.bottom,
 	g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-    
+
     //height and width of svg's
-    
+
     // parse by year-month-day
     var parseTime = d3.timeParse("%Y-%m-%d");
-    
+
     var scaleTime = d3.scaleTime().rangeRound([0,svg_width]);
-    
+
     var scaleStock = d3.scaleLinear().rangeRound([svg_height, 0]);
     var scaleSocial = d3.scaleLinear().rangeRound([svg_height, 0]);
-    
+
     var stockLine = d3.line()
 	.x(function(d) { return scaleTime(d.Date);} )
 	.y(function(d) { return scaleStock(d.Close);});
-    
+
     var trendLine =d3.line()
 	.x(function(d) { return scaleTime(d.Date);} )
 	.y(function(d) { return scaleSocial(d.Popularity);});
-    
-    // queue data for loading here 
+
+    // queue data for loading here
     d3.queue()
 	.defer( d3.csv, 'data/'+FileName+'_STOCK.csv' )
 	.defer( d3.csv, 'data/'+FileName+'_GOOGLE_TRENDS.csv' )
 	.await( function(error, data_stock, data_google_trends ) {
-	    
+
 	    data_google_trends=data_google_trends.reverse();
 	    // process data here!
 	    var dates = [];
-	    
+
 	    for (d of data_stock){
 		d.Date = parseTime(d.Date);
 		dates.push(d.Date);
 		d.Close = +d.Close;
 	    }
-	    
+
 	    for (d of data_google_trends){
 		d.Date = parseTime(d.Date);
 		dates.push(d.Date);
 		d.Popularity = +d.Popularity;
-	    }      
-	    
-	    
+	    }
+
+
 	    scaleTime.domain(d3.extent(dates, function(d) { return d; }));
-	    
+
 	    scaleStock.domain(d3.extent(data_stock, function(d) { return d.Close; }));
 	    scaleSocial.domain(d3.extent(data_google_trends, function(d) { return d.Popularity; }));
-	    
+
 	    g.append("g")
 		.attr("transform", "translate(0," + svg_height + ")")
 		.call(d3.axisBottom(scaleTime))
 		.select(".domain");
-	    
+
 	    g.append("g")
 		.call(d3.axisLeft(scaleStock))
 		.append("text")
@@ -97,7 +97,7 @@ function displayData(FileName) {
 		.attr("dy", "0.71em")
 		.attr("text-anchor", "end")
 		.text("Price ($)");
-	    
+
 	    g.append("g")
 		.call(d3.axisRight(scaleSocial))
       		.attr("transform", "translate(" + svg_width + ")")
@@ -108,7 +108,7 @@ function displayData(FileName) {
 		.attr("dy", "0.71em")
 		.attr("text-anchor", "end")
 		.text("Social");
-	    
+
 	    g.append("path")
 		.datum(data_google_trends)
 		.attr("fill", "none")
@@ -117,7 +117,7 @@ function displayData(FileName) {
 		.attr("stroke-linecap", "round")
 		.attr("stroke-width", 1.5)
 		.attr("d", trendLine);
-	    
+
 	    g.append("path")
 		.datum(data_stock)
 		.attr("fill", "none")
@@ -126,9 +126,6 @@ function displayData(FileName) {
 		.attr("stroke-linecap", "round")
 		.attr("stroke-width", 1.5)
 		.attr("d", stockLine);
-	    
+
 	});
 }
-
-
-
