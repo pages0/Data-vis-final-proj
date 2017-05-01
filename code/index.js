f79f18823987f4d0b42f3034db26fcad74d8e9ae
Connor G-T <CGT@Connors-MacBook-Pro-4.local>
2017-05-01 15:44:25 -0500

    Fixed scaling

--- code/index.js
+++ code/index.js
@@ -4,128 +4,146 @@
 displayData('AMAZON');
 
 d3.queue()
-    .defer( d3.csv, 'data/NAMES.csv' )
-    .await(function(error , names) {
-	var dropDown =
-	    d3.select('#filters')
-	    .append('select')
-	    .attr('id','companies')
-	    .on('change',function() {
-		displayData(d3.select(this).node().value);
-	    });
+.defer( d3.csv, 'data/NAMES.csv' )
+.await(function(error , names) {
+  var dropDown =
+  d3.select('#filters')
+  .append('select')
+  .attr('id','companies')
+  .on('change',function() {
+    displayData(d3.select(this).node().value);
+  });
 
-	dropDown.selectAll('option')
-	    .data(names)
-	    .enter()
-	    .append('option')
-	    .attr('value', function(d){
-		return d.FileName;
-	    }).text(function(d){
-		return d.Name;
-	    });
-    });
+  dropDown.selectAll('option')
+  .data(names)
+  .enter()
+  .append('option')
+  .attr('value', function(d){
+    return d.FileName;
+  }).text(function(d){
+    return d.Name;
+  });
+});
 
 
 
 function displayData(FileName) {
-    d3.select("svg").remove();
-    d3.select('#svg_area').append("svg").attr("width", 800).attr("height",450);
+  d3.select("svg").remove();
+  d3.select('#svg_area').append("svg").attr("width", 800).attr("height",450);
 
-    var svg = d3.select("svg"),
-	margin = {top: 20, right: 50, bottom: 30, left: 50},
-	svg_width = +svg.attr("width") - margin.left - margin.right,
-	svg_height = +svg.attr("height") - margin.top - margin.bottom,
-	g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  var svg = d3.select("svg"),
+  margin = {top: 20, right: 50, bottom: 30, left: 50},
+  svg_width = +svg.attr("width") - margin.left - margin.right,
+  svg_height = +svg.attr("height") - margin.top - margin.bottom,
+  g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    //height and width of svg's
+  // parse by year-month-day
+  var parseTime = d3.timeParse("%Y-%m-%d");
 
-    // parse by year-month-day
-    var parseTime = d3.timeParse("%Y-%m-%d");
+  var scaleTime = d3.scaleTime().rangeRound([0,svg_width]);
 
-    var scaleTime = d3.scaleTime().rangeRound([0,svg_width]);
+  var scaleStock = d3.scaleLinear().rangeRound([svg_height, 0]);
+  var scaleSocial = d3.scaleLinear().rangeRound([svg_height, 0]);
 
-    var scaleStock = d3.scaleLinear().rangeRound([svg_height, 0]);
-    var scaleSocial = d3.scaleLinear().rangeRound([svg_height, 0]);
+  var stockLine = d3.line()
+  .x(function(d) { return scaleTime(d.Date);} )
+  .y(function(d) { return scaleStock(d.Close);});
 
-    var stockLine = d3.line()
-	.x(function(d) { return scaleTime(d.Date);} )
-	.y(function(d) { return scaleStock(d.Close);});
+  var trendLine =d3.line()
+  .x(function(d) { return scaleTime(d.Date);} )
+  .y(function(d) { return scaleSocial(d.Popularity);});
 
-    var trendLine =d3.line()
-	.x(function(d) { return scaleTime(d.Date);} )
-	.y(function(d) { return scaleSocial(d.Popularity);});
+  // queue data for loading here
+  d3.queue()
+  .defer( d3.csv, 'data/'+FileName+'_STOCK.csv' )
+  .defer( d3.csv, 'data/'+FileName+'_GOOGLE_TRENDS.csv' )
+  .await( function(error, data_stock, data_google_trends ) {
 
-    // queue data for loading here
-    d3.queue()
-	.defer( d3.csv, 'data/'+FileName+'_STOCK.csv' )
-	.defer( d3.csv, 'data/'+FileName+'_GOOGLE_TRENDS.csv' )
-	.await( function(error, data_stock, data_google_trends ) {
-
-	    data_google_trends=data_google_trends.reverse();
-	    // process data here!
-	    var dates = [];
-
-	    for (d of data_stock){
-		d.Date = parseTime(d.Date);
-		dates.push(d.Date);
-		d.Close = +d.Close;
-	    }
-
-	    for (d of data_google_trends){
-		d.Date = parseTime(d.Date);
-		dates.push(d.Date);
-		d.Popularity = +d.Popularity;
-	    }
+    data_google_trends=data_google_trends.reverse();
+    // process data here!
+    var dates = [];
 
 
-	    scaleTime.domain(d3.extent(dates, function(d) { return d; }));
+    for (d of data_stock){
+      d.Date = parseTime(d.Date);
+      dates.push(d.Date);
+      d.Close = +d.Close;
+    }
 
-	    scaleStock.domain(d3.extent(data_stock, function(d) { return d.Close; }));
-	    scaleSocial.domain(d3.extent(data_google_trends, function(d) { return d.Popularity; }));
+    for (d of data_google_trends){
+      d.Date = parseTime(d.Date);
+      dates.push(d.Date);
+      d.Popularity = +d.Popularity;
+    }
 
-	    g.append("g")
-		.attr("transform", "translate(0," + svg_height + ")")
-		.call(d3.axisBottom(scaleTime))
-		.select(".domain");
+    // min and max dates
+    var maxDate_google_trends = d3.max(data_google_trends, function(d) { return d.Date });
+    var maxDate_data_stock = d3.max(data_stock, function(d) { return d.Date });
+    var minDate_google_trends = d3.min(data_google_trends, function(d) { return d.Date });
+    var minDate_data_stock = d3.min(data_stock, function(d) { return d.Date });
 
-	    g.append("g")
-		.call(d3.axisLeft(scaleStock))
-		.append("text")
-		.attr("fill", "#000")
-		.attr("transform", "rotate(-90)")
-		.attr("y", 6)
-		.attr("dy", "0.71em")
-		.attr("text-anchor", "end")
-		.text("Price ($)");
+    var minExtent = d3.max([minDate_google_trends,minDate_data_stock]);
+    var maxExtent = d3.min([maxDate_google_trends,maxDate_data_stock]);
 
-	    g.append("g")
-		.call(d3.axisRight(scaleSocial))
-      		.attr("transform", "translate(" + svg_width + ")")
-		.append("text")
-		.attr("fill", "#000")
-		.attr("transform", "rotate(-90)")
-		.attr("y", 6)
-		.attr("dy", "0.71em")
-		.attr("text-anchor", "end")
-		.text("Social");
+    data_google_trends=data_google_trends.filter(
+      function(d)
+      {
+        return d.Date < maxExtent && d.Date > minExtent;
+      });
+      data_stock=data_stock.filter(
+        function(d)
+        {
+          return d.Date < maxExtent && d.Date > minExtent;
+        });
 
-	    g.append("path")
-		.datum(data_google_trends)
-		.attr("fill", "none")
-		.attr("stroke", "green")
-		.attr("stroke-linejoin", "round")
-		.attr("stroke-linecap", "round")
-		.attr("stroke-width", 1.5)
-		.attr("d", trendLine);
+        // Scales the graph
+        scaleTime.domain([minExtent, maxExtent]);
+        scaleStock.domain(d3.extent(data_stock, function(d) { return d.Close; }));
+        scaleSocial.domain(d3.extent(data_google_trends, function(d) { return d.Popularity; }));
 
-	    g.append("path")
-		.datum(data_stock)
-		.attr("fill", "none")
-		.attr("stroke", "steelblue")
-		.attr("stroke-linejoin", "round")
-		.attr("stroke-linecap", "round")
-		.attr("stroke-width", 1.5)
-		.attr("d", stockLine);
+        g.append("g")
+        .attr("transform", "translate(0," + svg_height + ")")
+        .call(d3.axisBottom(scaleTime))
+        .select(".domain");
 
-	});
-}
+        g.append("g")
+        .call(d3.axisLeft(scaleStock))
+        .append("text")
+        .attr("fill", "#000")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", "0.71em")
+        .attr("text-anchor", "end")
+        .text("Price ($)");
+
+        g.append("g")
+        .call(d3.axisRight(scaleSocial))
+        .attr("transform", "translate(" + svg_width + ")")
+        .append("text")
+        .attr("fill", "#000")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", "0.71em")
+        .attr("text-anchor", "end")
+        .text("Social");
+
+        g.append("path")
+        .datum(data_google_trends)
+        .attr("fill", "none")
+        .attr("stroke", "green")
+        .attr("stroke-linejoin", "round")
+        .attr("stroke-linecap", "round")
+        .attr("stroke-width", 1.5)
+        .attr("d", trendLine);
+
+        g.append("path")
+        .datum(data_stock)
+        .attr("fill", "none")
+        .attr("stroke", "steelblue")
+        .attr("stroke-linejoin", "round")
+        .attr("stroke-linecap", "round")
+        .attr("stroke-width", 1.5)
+        .attr("d", stockLine);
+
+      });
+    }
